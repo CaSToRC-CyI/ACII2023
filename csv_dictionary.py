7791dd103fd31b2741fef62a33227483e74829b6
ValeriaF22 <v.filippou22@gmail.com>
2023-07-13 17:16:16 +0300

    Update csv_dictionary.py

--- csv_dictionary.py
+++ csv_dictionary.py
@@ -9,14 +9,6 @@ from acii2023_functions import create_info_dictionary
 
 # csv file path
 csv_file = '/Users/user/Desktop/ACII2023_Datasets/alexithymia_demographics_info.csv'
-    
-#%%
-
-# ID would be the key column of the dictionary
-key_column = 'id'
-
-# Group and dataset labels would be the values of the dictionary
-value_columns = ['group', 'dataset'] 
 
 # Dictionary containing information for each participant
-info_dict = create_info_dictionary(csv_file, key_column, value_columns)
+info_dict = create_info_dictionary(csv_file, 'id', ['group', 'dataset', 'TASDIF', 'TASDDF', 'TASEOT', 'TAStot'])
