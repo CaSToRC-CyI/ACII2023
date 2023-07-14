5a7e136953efa5468ce023d00f242583e95d5ca0
ValeriaF22 <v.filippou22@gmail.com>
2023-07-14 13:42:38 +0300

    Update dataset2_medium_dictionaries.py

--- dataset2_medium_dictionaries.py
+++ dataset2_medium_dictionaries.py
@@ -11,13 +11,21 @@ from load_data import load_participant_data
 
 folder_path = '/Users/user/Desktop/ACII2023_Datasets/Dataset2/MediumAlexithymics/'
 #%%
-participant_112307 = load_participant_data(folder_path, '112307.acq', info_dict, normalise=True, downsample=True)
-participant_112353 = load_participant_data(folder_path, '112353.acq', info_dict, normalise=True, downsample=True)
-participant_122324 = load_participant_data(folder_path, '122324.acq', info_dict, normalise=True, downsample=True)
-participant_221311 = load_participant_data(folder_path, '221311.acq', info_dict, normalise=True, downsample=True)
-participant_222320 = load_participant_data(folder_path, '222320.acq', info_dict, normalise=True, downsample=True)
-participant_222323 = load_participant_data(folder_path, '222323.acq', info_dict, normalise=True, downsample=True)
-participant_222332 = load_participant_data(folder_path, '222332.acq', info_dict, normalise=True, downsample=True)
-participant_311367 = load_participant_data(folder_path, '311367.acq', info_dict, normalise=True, downsample=True)
-participant_312355 = load_participant_data(folder_path, '312355.acq', info_dict, normalise=True, downsample=True)
-participant_321349 = load_participant_data(folder_path, '321349.acq', info_dict, normalise=True, downsample=True)
+
+# Load data of participants individually
+participant_112307 = load_participant_data(folder_path, '112307.acq', info_dict, normalise=True, downsample=True, state='Universal')
+participant_112353 = load_participant_data(folder_path, '112353.acq', info_dict, normalise=True, downsample=True, state='Universal')
+participant_122324 = load_participant_data(folder_path, '122324.acq', info_dict, normalise=True, downsample=True, state='Universal')
+participant_221311 = load_participant_data(folder_path, '221311.acq', info_dict, normalise=True, downsample=True, state='Universal')
+participant_222320 = load_participant_data(folder_path, '222320.acq', info_dict, normalise=True, downsample=True, state='Universal')
+participant_222323 = load_participant_data(folder_path, '222323.acq', info_dict, normalise=True, downsample=True, state='Universal')
+participant_222332 = load_participant_data(folder_path, '222332.acq', info_dict, normalise=True, downsample=True, state='Universal')
+participant_311367 = load_participant_data(folder_path, '311367.acq', info_dict, normalise=True, downsample=True, state='Universal')
+participant_312355 = load_participant_data(folder_path, '312355.acq', info_dict, normalise=True, downsample=True, state='Universal')
+participant_321349 = load_participant_data(folder_path, '321349.acq', info_dict, normalise=True, downsample=True, state='Universal')
+
+#%% 
+
+# List of participants to load dataset
+dataset2_medium = ['222320.acq', '112307.acq', '312355.acq', '222323.acq', '222332.acq', '321349.acq',\
+                   '311367.acq', '221311.acq', '122324.acq', '112353.acq']
